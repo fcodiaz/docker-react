7c14ff50c285405ca455fd2b931f0b000ef6149c
Francisco Diaz <fcodiaz@gmail.com>
2021-10-11 11:29:31 -0400

    Added the project and travis file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey this is me!!! and Me too!!!
+        </p>
+        <p>
+	  I just added a new line
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
