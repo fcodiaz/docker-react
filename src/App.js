080eec4a3ad44eb0a661f26348a0b81eff4a1472
Francisco Diaz <fcodiaz@gmail.com>
2022-07-28 12:32:52 -0400

    Updated something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey this is me!!! and Me too!!!
+          Hey this is me!!! and Me too!!! And also me!!!
         </p>
         <p>
 	  I just added a new line
